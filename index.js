5b0b91787b08931cd7b5649cdd030a4b883085a9
khai96_ <hvksmr1996@gmail.com>
2016-08-10 23:22:41 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const {readdirSync, statSync} = require('fs')
+
+function getAllItems (path) {
+
+}
+
+function getItemsByClasses (path, {
+  required = [], optional = [],
+  prefix = '', suffix = ''
+} = {}) {
+  
+}
+
+module.exports = {
+  getAllItems,
+  getItemsByClasses
+}
