472fc621449d9aa26a56e9769e85f1699afed69e
Khải <hvksmr1996@gmail.com>
2016-08-11 21:44:21 +0700

    always fail travis

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 standard && {
 	echo Result of \`require\(\'$(pwd)\'\)\`
 	node -p "require('.')"
+} && {
+	echo You have passed 2 tests but you still fucking fail! Look at the exit code, awesome, isn't it?!
+	exit 1234
 }
