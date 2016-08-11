b4872b7750c13134d0668e8b5bdc486bca47f772
Khải <hvksmr1996@gmail.com>
2016-08-11 21:53:54 +0700

    correct my awesome message

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ standard && {
 	echo Result of \`require\(\'$(pwd)\'\)\`
 	node -p "require('.')"
 } && {
-	echo You have passed 2 tests but you still fucking fail! Look at the exit code, awesome, isn't it?!
+	echo "You have passed 2 tests but you still fucking fail! Look at the exit code, awesome, isn't it?!"
 	exit 1234
 }
