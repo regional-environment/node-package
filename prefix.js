746a6d4910aee2781cd392c59c4b2773d9e55220
khai96_ <hvksmr1996@gmail.com>
2016-08-12 12:13:58 +0700

    improve prefix, suffix

--- prefix.js
+++ prefix.js
@@ -1,4 +1,4 @@
 'use strict'
 
 module.exports = (string, prefix) =>
-  !prefix || string.slice(0, prefix.length) === prefix
+  string.slice(0, prefix.length) === prefix
--- suffix.js
+++ suffix.js
@@ -1,4 +1,4 @@
 'use strict'
 
 module.exports = (string, suffix) =>
-  !suffix || string.slice(-suffix.length) === suffix
+  !suffix || string.slice(-suffix.length, string.length) === suffix
