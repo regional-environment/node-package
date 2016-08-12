af4eabc7c6f61da08005a009cb5749e283849f21
khai96_ <hvksmr1996@gmail.com>
2016-08-12 12:54:16 +0700

    improve tester

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
-standard && {
+(
+	echo Checking Code Style
+	standard && (
+		echo "Acceptable Code Style"
+	) || (
+		echo "Bad Code Style" >&2
+		exit 2
+	)
+) && (
 	echo Result of \`require\(\'$(pwd)\'\)\`
 	node -p "require('.')"
-}
+)
